bcd98eff28f8a77079783312522cc3b11e29c384
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-12 11:20:03 -0300

    feat: "setup documentation"

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('docs/', get_schema_view(title='Api Schema',description='Api Documentation'),name="docs")
+
 ]

b963a86a1b2db451d2a55e2c9b481f6f4874aa23
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-12 12:45:31 -0300

    feat: migration and model music

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'album',
     'artist',
-    # 'music',
+    'music',
     # 'music_album_artist',
 ]
 
--- backend/music/models.py
+++ backend/music/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from album.models import Album 
 # Create your models here.
+class Music(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    id_album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    duration = models.IntegerField()
+    is_favorite = models.BooleanField()
+
+    class Meta:
+        db_table = 'music'
+
+    def __str__(self) -> str:
+        return self.name

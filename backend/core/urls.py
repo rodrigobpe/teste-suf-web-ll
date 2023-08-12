630692652ffaee5dfaf8c9256af94ca3938dd60c
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-08-12 17:20:10 -0300

    alphabetical order

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -20,9 +20,9 @@ from rest_framework.schemas import get_schema_view
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url': 'api_schema'}), name='swagger-ui'),
     path('admin/', admin.site.urls),
     path('api_schema/', get_schema_view(title='API Schema', description='Guide for the REST API'), name='api_schema'),
-    path('', TemplateView.as_view(template_name='docs.html', extra_context={'schema_url': 'api_schema'}), name='swagger-ui'),
     path('api/', include('album.urls')),
     path('api/', include('artist.urls')),
     path('api/', include('music.urls')),

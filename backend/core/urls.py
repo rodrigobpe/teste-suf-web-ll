8a11ccbd2a40a39ee110ab14d78338fc58c886fd
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-12 13:30:47 -0300

    feat: swagger ui docs

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rest_framework.schemas import get_schema_view
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('docs/', get_schema_view(title='Api Schema',description='Api Documentation'),name="docs")
-
+    path('api_schema/', get_schema_view(title='API Schema',description='Guide for the REST API'), name='api_schema'),
+    path('docs/', TemplateView.as_view(template_name='docs.html',extra_context={'schema_url':'api_schema'}), name='swagger-ui'),
 ]

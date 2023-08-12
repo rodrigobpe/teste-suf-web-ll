044c6a24a240729cf5bb60178fe1613f6861d323
Marcus Vinicius da Costa Graciano <marcusviniciusgraciano04@gmail.com>
2023-08-12 12:51:23 -0300

    right formatting

--- backend/music/models.py
+++ backend/music/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-from album.models import Album 
+from album.models import Album
+
+
 # Create your models here.
 class Music(models.Model):
     id = models.BigAutoField(primary_key=True)

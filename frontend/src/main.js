5aec647f0c19908eecbca53d5584fcd3c07eeebd
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-12 16:37:03 -0300

    feat setup vue router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
+import router from './router'
+import {createPinia} from 'pinia'
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .mount('#app')
---
+++ frontend/src/router/index.js
@@ -0,0 +1,17 @@
+import {createRouter,createWebHistory} from 'vue-router'
+import HomeView from '../views/HomeView/index.vue'
+const routes = [
+    {
+        path:'/',
+        component: HomeView 
+    }
+]
+
+const router = createRouter(
+    {
+        history:createWebHistory(),
+        routes
+    }
+)
+
+export default router

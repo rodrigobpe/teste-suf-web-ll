4db514dc11d193c49c0335c0b1a08d90f71afe33
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-13 20:23:45 -0300

    refactor: arrumando os erros da documentação

--- backend/src/modules/album/album.controller.ts
+++ backend/src/modules/album/album.controller.ts
@@ -13,13 +13,14 @@ export class AlbumController {
     @Post('artists/:id/albums')
     @ApiParam({ name: "id", example: 1, type: Number })
     @ApiCreatedResponse({ status: 201, description: "The record has been successfully created.", type: Album })
-    async createAlbum(@Param('id',ParseIntPipe) id_artist:number, @Body() { name,date_release,genre,is_favorite }: CreateAlbumDTO) {
-        const album = await this.artistService.create({ name,date_release:new Date(date_release),genre,is_favorite,id_artist })
+    async createAlbum(@Param('id', ParseIntPipe) id_artist: number, @Body() { name, date_release, genre, is_favorite }: CreateAlbumDTO) {
+        const album = await this.artistService.create({ name, date_release: new Date(date_release), genre, is_favorite, id_artist })
         return album
     }
 
     @Get('albums')
     @ApiCreatedResponse({ status: 200, type: Album, isArray: true })
+    @ApiNotFoundResponse({ description: "Not Found", schema: { example: { statusCode: 404, message: "Não existem albums cadastrados" } } })
     async getAllAlbums() {
         const albums = await this.artistService.getAll()
         return albums
@@ -28,8 +29,8 @@ export class AlbumController {
     @Get('/albums/:id')
     @ApiParam({ name: "id", example: 1, type: Number })
     @ApiOkResponse({ status: 200, type: Album })
-    @ApiNotFoundResponse({ description: "Not found" },)
-    async getAlbumById(@Param('id',ParseIntPipe) id:number ) {
+    @ApiNotFoundResponse({ description: "Not found", schema: { example: { statusCode: 404, message: "Album não existe" } } })
+    async getAlbumById(@Param('id', ParseIntPipe) id: number) {
         const album = await this.artistService.getById({ id })
         return album
     }
@@ -37,26 +38,26 @@ export class AlbumController {
     @Get('artists/:id/albums')
     @ApiParam({ name: "id", example: 1, type: Number })
     @ApiOkResponse({ status: 200, type: Album })
-    @ApiNotFoundResponse({ description: "Not found" },)
-    async getAlbumByArtists(@Param('id',ParseIntPipe) id_artist:number ) {
+    @ApiNotFoundResponse({ description: "Not found", schema: { example: { statusCode: 404, message: "O artista não possui nenhum album" } } },)
+    async getAlbumByArtists(@Param('id', ParseIntPipe) id_artist: number) {
         const album = await this.artistService.getByArtist({ id_artist })
         return album
     }
 
     @Delete('/albums/:id')
     @ApiParam({ name: "id", example: 1, type: Number })
-    @ApiOkResponse({ status: 200, description: "Artista deletado com sucesso" })
-    @ApiNotFoundResponse({ description: "Not found" })
-    async deleteAlbum(@Param('id',ParseIntPipe) id:number) {
-        const album = await this.artistService.delete({ id })
-        return album
+    @ApiOkResponse({ status: 200, description: "Album deletado com sucesso", schema: { example: { statusCode: 200, message: "Album com o ID X foi deletado" }, } })
+    @ApiNotFoundResponse({ description: "Not found",schema:{example:{statusCode:404,message:"Album não existe"}} })
+    async deleteAlbum(@Param('id', ParseIntPipe) id: number) {
+        await this.artistService.delete({ id })
+        return { statusCode: 200, message: `Album com o ID ${id} foi deletado` }
     }
 
     @Patch('/albums/:id')
-    @ApiParam({ name: "id", example: 1,type:Number })
+    @ApiParam({ name: "id", example: 1, type: Number })
     @ApiOkResponse({ status: 200, description: "Artista modificado com sucesso", type: Album })
     @ApiNotFoundResponse({ description: "Not found" })
-    async updateAlbum(@Param('id',ParseIntPipe) id:number, @Body() { is_favorite }: UpdateAlbumtDTO) {
+    async updateAlbum(@Param('id', ParseIntPipe) id: number, @Body() { is_favorite }: UpdateAlbumtDTO) {
         const album = await this.artistService.update({ id, is_favorite })
         return album
     }
--- backend/src/modules/artist/artist.controller.ts
+++ backend/src/modules/artist/artist.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Param, Delete, Patch, ParseIntPipe } from '@nestjs/common'
+import { Body, Controller, Get, Post, Param, Delete, Patch, ParseIntPipe, HttpException } from '@nestjs/common'
 import { ArtistService } from './artist.service'
 import { CreateArtistDTO } from './dto/create-artist.dto'
 import { ApiCreatedResponse, ApiTags, ApiHeader, ApiNotFoundResponse, ApiParam, ApiOkResponse } from '@nestjs/swagger'
@@ -19,6 +19,7 @@ export class ArtistController {
 
     @Get()
     @ApiCreatedResponse({ status: 200, type: Artist, isArray: true })
+    @ApiNotFoundResponse({description:"Not found",schema:{example:{statusCode:404,message:"Não existem artistas cadastrados"}}})
     async getAllArtists() {
         const artists = await this.artistService.getAll()
         return artists
@@ -27,7 +28,7 @@ export class ArtistController {
     @Get('/:id')
     @ApiParam({ name: "id", example: 1, type: Number })
     @ApiOkResponse({ status: 200, type: Artist })
-    @ApiNotFoundResponse({ description: "Not found" },)
+    @ApiNotFoundResponse({ description: "Not found",schema:{example:{statusCode:404,message:"Artista não existe"},}})
     async getArtistsById(@Param('id',ParseIntPipe) id:number ) {
         const artist = await this.artistService.getById({ id })
         return artist
@@ -35,17 +36,17 @@ export class ArtistController {
 
     @Delete('/:id')
     @ApiParam({ name: "id", example: 1, type: Number })
-    @ApiOkResponse({ status: 200, description: "Artista deletado com sucesso" })
-    @ApiNotFoundResponse({ description: "Not found" })
+    @ApiOkResponse({ status: 200, description: "Artista deletado com sucesso",schema:{example:{statusCode:200,message:"Artista com o ID: X foi deletado"}} })
+    @ApiNotFoundResponse({ description: "Not found",schema:{example:{statusCode:404,message:"Artista não existe"},}})
     async deleteArtist(@Param('id',ParseIntPipe) id:number) {
-        const artist = await this.artistService.delete({ id })
-        return artist
+        await this.artistService.delete({ id })
+        return {statusCode:200,message:`Artista com o ID: ${id} foi deletado`}
     }
 
     @Patch('/:id')
     @ApiParam({ name: "id", example: 1,type:Number })
     @ApiOkResponse({ status: 200, description: "Artista modificado com sucesso", type: Artist })
-    @ApiNotFoundResponse({ description: "Not found" })
+    @ApiNotFoundResponse({ description: "Not found",schema:{example:{statusCode:404,message:"Artista não existe"},}})
     async updateArtist(@Param('id',ParseIntPipe) id:number, @Body() { name }: UpdateArtistDTO) {
         const artist = await this.artistService.update({ id, name })
         return artist

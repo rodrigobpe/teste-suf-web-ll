55c57b51b46a886d6b8d8d42d38a89bf9ce88b68
rodrigobpe <rodrigobatistapereira2@gmail.com>
2023-08-13 15:12:46 -0300

    feat: criando configuração do prisma module e arrumando comando de dev no package.json

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
-import {ConfigModule} from '@nestjs/config'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaModule } from './config/db/prisma.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
-    isGlobal:true
-  })],
+    isGlobal: true
+  }),
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
